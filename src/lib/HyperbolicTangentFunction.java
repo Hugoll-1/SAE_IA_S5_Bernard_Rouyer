c658082e0c59166eec0a9d9753ee0b8a48151e5d
Hugoll-1 <hugolin.rouyer9@etu.univ-lorraine.fr>
2023-12-11 09:24:42 +0100

    Ajout des fonctions de transferts

---
+++ src/lib/HyperbolicTangentFunction.java
@@ -0,0 +1,28 @@
+package lib;
+
+public class HyperbolicTangentFunction implements TransferFunction {
+
+    /**
+     * Fonction tangente hyperbolique
+     *
+     * @param value Entrée
+     * @return Sortie de la fonction tangente hyperbolique sur l'entrée
+     */
+    @Override
+    public double evaluate(double value) {
+        return Math.tanh(value);
+    }
+
+    /**
+     * Dérivée de la fonction tangente hyperbolique
+     *
+     * @param value Entrée
+     * @return Sortie de la dérivée de la fonction tangente hyperbolique sur l'entrée
+     */
+    @Override
+    public double evaluateDer(double value) {
+        double tanh = evaluate(value);
+        return 1.0 - tanh * tanh;
+    }
+}
+
