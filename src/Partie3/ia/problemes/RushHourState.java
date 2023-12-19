995b50c6c486f0d8255274b27359602f156934fb
Julien Bernard <julienbernardjb88@gmail.com>
2023-12-19 16:32:54 +0100

    Amélioration heuristique RushHour

--- src/Partie3/ia/problemes/RushHourState.java
+++ src/Partie3/ia/problemes/RushHourState.java
@@ -14,7 +14,7 @@ import static Partie3.ia.problemes.RushHour.DOWN;
 
 public class RushHourState extends State implements HasHeuristic {
 
-    private int difficulte = 1;
+    private int difficulte = 2;
 
     // Représentation des véhicules
     private int[][] carte;
@@ -61,7 +61,7 @@ public class RushHourState extends State implements HasHeuristic {
         };
         this.listeNumVoiture = switch (difficulte) {
             case 1 -> new int[]{1, 2, 3, 4, 5};
-            case 2,3 -> new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            case 2, 3 -> new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
             case 4 -> new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
             default -> throw new IllegalStateException("Unexpected value: " + difficulte);
         };
@@ -97,7 +97,6 @@ public class RushHourState extends State implements HasHeuristic {
     }
 
 
-
     @Override
     public boolean equals(Object o) {
         return Arrays.deepEquals(carte, ((RushHourState) o).carte);
@@ -108,17 +107,100 @@ public class RushHourState extends State implements HasHeuristic {
         return Arrays.deepHashCode(carte);
     }
 
+    //Heuristique v1: calcul distance entre voiture rouge et arrivée
+
+    // Heuristique v2
+//    @Override
+//    public double getHeuristic() {
+//        // Distance entre la voiture 1 et la sortie + estimation du cout de libération
+//        int heuristique = 0;
+//        int ligne = coorDonneesSortie[0];
+//        int colonne = coorDonneesSortie[1];
+//        while (getValueAt(ligne, colonne) != 1) {
+//            heuristique++;
+//            //Estimation du cout de liberation de la case (ligne, colonne)
+//            if (getValueAt(ligne, colonne) != 0) {
+//                heuristique++;
+//                // Vérification des cases au dessus et en dessous de la case pour estimer en combien de coûp on peut libérer la case
+//                //Premier ligne
+//                int valeurLigne0 = getValueAt(0,colonne);
+//
+//                int valeurLigne1 = getValueAt(1,colonne);
+//                //Ligne de sortie
+//                int valeurLigne2 = getValueAt(2,colonne);
+//
+//                int valeurLigne3 = getValueAt(3,colonne);
+//                int valeurLigne4 = getValueAt(4,colonne);
+//
+//                int valeurDegagementHaut;
+//                if(valeurLigne0 == 0 && valeurLigne1 == 0){
+//                    valeurDegagementHaut = 2;
+//                }else if (valeurLigne0 == 0 && valeurLigne1 == valeurLigne2){
+//                    valeurDegagementHaut = 1;
+//                } else if (valeurLigne0 != 0 && valeurLigne1 == valeurLigne2){
+//                    valeurDegagementHaut = 2;
+//                }else if (valeurLigne0 == 0 || valeurLigne1 == 0){
+//                    valeurDegagementHaut = 3;
+//                }else valeurDegagementHaut = 4;
+//
+//                int valeurDegagementBas;
+//                if(valeurLigne3 == 0 && valeurLigne4 == 0){
+//                    valeurDegagementBas = 2;
+//                } else if (valeurLigne4== 0 && valeurLigne2== valeurLigne3) {
+//                    valeurDegagementBas = 1;
+//                } else if (valeurLigne4 != 0 && valeurLigne2== valeurLigne3) {
+//                    valeurDegagementBas = 2;
+//                } else if (valeurLigne4 == 0 || valeurLigne3 == 0) {
+//                    valeurDegagementBas = 3;
+//                } else valeurDegagementBas = 4;
+//                //On ajoute à l'heuristique la plus petite valeur entre valeurDegagementBas et valeurDegagementHaut
+//                heuristique += Math.min(valeurDegagementBas,valeurDegagementHaut);
+//            }
+//
+//            colonne--;
+//        }
+//        return heuristique;
+//    }
+
+    // Heuristique v3
     @Override
     public double getHeuristic() {
+        int heuristique = 0;
+
         // Distance entre la voiture 1 et la sortie
-        int distance = 0;
         int ligne = coorDonneesSortie[0];
         int colonne = coorDonneesSortie[1];
         while (getValueAt(ligne, colonne) != 1) {
-            distance++;
+            heuristique += 1;
+
+            // Estimation du coût de libération
+            if (getValueAt(ligne, colonne) != 0) {
+                heuristique += coutLiberation(ligne, colonne);
+            }
+
             colonne--;
         }
-        return distance;
+
+        return heuristique;
+    }
+
+    private int coutLiberation(int ligne, int colonne) {
+        int valeurDegagementHaut = coutLiberationDirection(ligne, colonne, -1);
+        int valeurDegagementBas = coutLiberationDirection(ligne, colonne, 1);
+
+        return Math.min(valeurDegagementHaut, valeurDegagementBas);
+    }
+
+    private int coutLiberationDirection(int ligne, int colonne, int direction) {
+        int valeurVoisin = getValueAt(ligne + direction, colonne);
+
+        if (valeurVoisin == 0) {
+            return 3;
+        } else if (valeurVoisin == getValueAt(ligne + 2 * direction, colonne)) {
+            return 2;
+        } else {
+            return 5;
+        }
     }
 
     public String toString() {
@@ -149,7 +231,7 @@ public class RushHourState extends State implements HasHeuristic {
             } else {
                 // Vérification de l'orientation de la voiture
                 // On retourne faux si la case est tout a droite ou si la case de droite n'est pas identitque au véhicule
-                if(coordonnees[1] == 5 || carte[coordonnees[0]][coordonnees[1]+1]!=valeur) return false;
+                if (coordonnees[1] == 5 || carte[coordonnees[0]][coordonnees[1] + 1] != valeur) return false;
                 return valeurVoisin == 0;
             }
         }
@@ -160,7 +242,7 @@ public class RushHourState extends State implements HasHeuristic {
                 return isLegal(new int[]{coordonnees[0], coordonnees[1] + 1}, a);
             } else {
                 // Vérification de l'orientation de la voiture
-                if(coordonnees[1] == 0 || carte[coordonnees[0]][coordonnees[1]-1]!=valeur) return false;
+                if (coordonnees[1] == 0 || carte[coordonnees[0]][coordonnees[1] - 1] != valeur) return false;
                 return valeurVoisin == 0;
             }
         }
@@ -170,7 +252,7 @@ public class RushHourState extends State implements HasHeuristic {
             if (valeurVoisin == valeur) {
                 return isLegal(new int[]{coordonnees[0] - 1, coordonnees[1]}, a);
             } else {
-                if (coordonnees[0] == 5 || carte[coordonnees[0]+1][coordonnees[1]]!=valeur) return false;
+                if (coordonnees[0] == 5 || carte[coordonnees[0] + 1][coordonnees[1]] != valeur) return false;
                 return valeurVoisin == 0;
             }
         }
@@ -180,7 +262,7 @@ public class RushHourState extends State implements HasHeuristic {
             if (valeurVoisin == valeur) {
                 return isLegal(new int[]{coordonnees[0] + 1, coordonnees[1]}, a);
             } else {
-                if (coordonnees[0] == 0 || carte[coordonnees[0]-1][coordonnees[1]]!=valeur) return false;
+                if (coordonnees[0] == 0 || carte[coordonnees[0] - 1][coordonnees[1]] != valeur) return false;
                 return valeurVoisin == 0;
             }
         }
@@ -217,7 +299,7 @@ public class RushHourState extends State implements HasHeuristic {
         // Suppression de la queue de la voiture
         ligne = vehicule[0];
         colonne = vehicule[1];
-        while (colonne > 0 && carte[ligne][colonne-1] == valeur) {
+        while (colonne > 0 && carte[ligne][colonne - 1] == valeur) {
             colonne--;
         }
         carte[ligne][colonne] = 0;
@@ -240,7 +322,7 @@ public class RushHourState extends State implements HasHeuristic {
 
         ligne = vehicule[0];
         colonne = vehicule[1];
-        while (colonne < 5 && carte[ligne][colonne+1] == valeur) {
+        while (colonne < 5 && carte[ligne][colonne + 1] == valeur) {
             colonne++;
         }
         carte[ligne][colonne] = 0;
@@ -263,7 +345,7 @@ public class RushHourState extends State implements HasHeuristic {
 
         ligne = vehicule[0];
         colonne = vehicule[1];
-        while (ligne < 5 && carte[ligne+1][colonne] == valeur) {
+        while (ligne < 5 && carte[ligne + 1][colonne] == valeur) {
             ligne++;
         }
         carte[ligne][colonne] = 0;
@@ -286,7 +368,7 @@ public class RushHourState extends State implements HasHeuristic {
 
         ligne = vehicule[0];
         colonne = vehicule[1];
-        while (ligne > 0 && carte[ligne-1][colonne] == valeur) {
+        while (ligne > 0 && carte[ligne - 1][colonne] == valeur) {
             ligne--;
         }
         carte[ligne][colonne] = 0;
